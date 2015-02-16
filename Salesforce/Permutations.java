f2ba38ec2e44c0cbbacb52dcc62d846f016cc6b5
Don Liang <duolingl@gmail.com>
2015-02-16 14:53:56 -0800

    modify permutations

--- Salesforce/Permutations.java
+++ Salesforce/Permutations.java
@@ -4,39 +4,38 @@
  * Now print the permutations in dictionary order.
  */
 
-public class solution {
-public List<List<Integer>> permute(int[] num) {
-    	List<List<Integer>> result = new ArrayList<List<Integer>>();
-    	permute(num, 0, result);
-    	return result;
-    }
-     
-    public void permute(int[] num, int start, List<List<Integer>> result) {
-     
-    	if (start >= num.length) {
-    		ArrayList<Integer> item = convertArrayToList(num);
-    		result.add(item);
-    	}
-     
-     	// Core code
-    	for (int j = start; j <= num.length - 1; j++) {
-    		swap(num, start, j);
-    		permute(num, start + 1, result);
-    		swap(num, start, j);
-    	}
-    }
-     
-    private ArrayList<Integer> convertArrayToList(int[] num) {
-    	ArrayList<Integer> item = new ArrayList<Integer>();
-    	for (int h = 0; h < num.length; h++) {
-    		item.add(num[h]);
-    	}
-    	return item;
-    }
-     
-    private void swap(int[] a, int i, int j) {
-    	int temp = a[i];
-    	a[i] = a[j];
-    	a[j] = temp;
-    }
+public class Solution {
+	public List<List<Integer>> permute(int[] num) {
+		List<List<Integer>> res = new ArrayList<List<Integer>>();
+		permuteHelper(num, 0, res);
+		return res;
+	}
+
+	public void permuteHelper(int[] num, int start, List<List<Integer>> result) {
+		if(start == num.length) {
+			List<Integer> item = convertArrayToList(num);
+			result.add(item);
+		}
+
+		for(int j = start; j < num.length; j++) {
+			swap(num, start, j);
+			permuteHelper(num, start + 1, result);
+			swap(num, start, j);
+		}
+	}
+
+	public List<Integer> convertArrayToList(int[] num) {
+		List<Integer> newList = new ArrayList<Integer>();
+		for(int i : num) {
+			newList.add(i);
+		}
+		return newList;
+	}
+
+	public void swap(int[] num, int start, int j) {
+		int temp = num[start];
+		num[start] = num[j];
+		num[j] = temp;
+	}
+
 }
